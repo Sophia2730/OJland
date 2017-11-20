3735f8e484099d78f2821e7dbb495ac081aa946c
stafor94 <stafor@naver.com>
2017-11-20 12:28:08 +0900

    UI 수정

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var main = require('./routes/main');
 var index = require('./routes/index');
 
 var app = express();
