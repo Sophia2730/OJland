72e2407c542def73ae494360ab768c233c8b50d2
stafor94 <stafor@naver.com>
2017-12-03 18:11:38 +0900

    통계 아이콘 추가, 크기 및 배치 수정

--- routes/admin/statistics.js
+++ routes/admin/statistics.js
@@ -6,7 +6,7 @@ var moment = require('moment');
 
 
 router.get('/', function(req, res, next) {
-    if (!req.session.Name) {  // 로그인 여부 체크
+    if (req.session.UserType != 'AD') {  // 관리자 로그인 여부 체크
         res.redirect('/');  // 세션이 없으면 메인 페이지로 이동
         return;
     }
