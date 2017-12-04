b27366207a2329ff256ce167873b3cad10ef0683
DESKTOP-ED08EH0\rladh <rladhtjddl@naver.com>
2017-12-05 00:30:55 +0900

    진척도조회(진척도 리스트)

--- routes/progress/progress_list.js
+++ routes/progress/progress_list.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pool = require('../../config.js').pool;
+var moment = require('moment');
 
 router.get('/', function(req, res, next) {
     var body = req.body;
@@ -20,6 +21,32 @@ router.get('/', function(req, res, next) {
         });
     });
 });
+
+
+router.post('/',function(req,res){
+  var body = req.body;
+  console.log("UID: " + body.uuid);
+  console.log("OID: "+ body.ooid);
+
+  pool.getConnection(function(err, connection) {
+
+          queryStr = "SELECT * FROM progress WHERE _UID=? AND _OID=?";
+          connection.query(queryStr, [body.uuid,body.ooid], function (err, rows) {
+              if(err) console.log("err: ", err);
+              var dates = [];
+              for (var i = 0; i < rows.length; i++) {
+                  dates[i] = moment(rows[i].Time).format('YYYY/MM/DD');
+              }
+              res.render('progress/progress_list', {
+                  data: rows,
+                  date: dates,
+                  session: req.session
+              });
+              connection.release();
+          });
+  });
+
+})
 
 
 module.exports = router;
