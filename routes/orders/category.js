fa39f7ad8c7e22c6ed074122593b3200292acd61
stafor94 <stafor@naver.com>
2017-12-05 14:20:08 +0900

    발주정보, 발주상세정보 UI 수정

--- routes/orders/category.js
+++ routes/orders/category.js
@@ -76,4 +76,12 @@ router.get('/:id', function(req, res, next) {
     res.redirect('/category');
 });
 
+router.get('/select', function(req, res, next) {
+
+});
+
+router.post('/select', function(req, res, next) {
+
+});
+
 module.exports = router;
