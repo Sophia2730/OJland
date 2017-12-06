80c92ecb431cc2ef05cb90c21e5649052ee0c409
ChanSu <sophia@ChanSuui-MacBook-Pro.local>
2017-12-06 22:58:26 +0900

    회원 가입내역 화면

--- routes/admin/statistics.js
+++ routes/admin/statistics.js
@@ -3,7 +3,15 @@ var router = express.Router();
 var pool = require('../../config.js').pool;
 var async = require('async');
 var moment = require('moment');
+var d = new Date();
 
+var d1 = d.getFullYear() + "-" + (d.getMonth() + 1);
+var d2 = d1 + "-" + d.getDate();
+if(d.getDate() < 10){
+    d2 = d1 + "-0" + d.getDate();
+} else {
+    d2 = d1 + "-" + d.getDate();
+}
 
 router.get('/', function(req, res, next) {
     if (req.session.UserType != 'AD') {  // 관리자 로그인 여부 체크
@@ -24,9 +32,23 @@ router.get('/', function(req, res, next) {
                     if(err) callback(err);
                     callback(null, rows);
                 });
+            },
+            function(callback) {
+                connection.query("SELECT count(*) as cnt FROM user WHERE Status=1 AND RegTime LIKE '%" + d1 +"%';", function(err, rows) {
+                    if(err) callback(err);
+                    callback(null, rows);
+                });
+            },
+            function(callback) {
+                connection.query("SELECT count(*) as cnt FROM user WHERE Status=1 AND RegTime LIKE '%" + d2 +"%';", function(err, rows) {
+                    if(err) callback(err);
+                    callback(null, rows);
+                });
             }
         ], function(err, results) {
             if (err) console.log('err: ', err);
+            console.log("res2", results[2], "d1", d1);
+            console.log("res3", results[3], "d2", d2);
             date = [0,0,0,0,0,0,0,0,0,0,0,0,0];
             for (var i = 0; i < results[1].length; i++) {
                 var a = parseInt(moment(results[1][i].Time).format('MM'))
@@ -36,12 +58,13 @@ router.get('/', function(req, res, next) {
             res.render('admin/statistics', {
                 user: results[0],
                 ojcnt: date,
-                session: req.session
+                session: req.session,
+                d1: results[3],
+                d2: results[2]
             });
             connection.release();
         });
     });
-
 });
 
 module.exports = router;
